dc08550cdcb7c0df335d6c2e4e321663463d4330
Ethan Brown <ethanmillerbrown@gmail.com>
2025-07-26 21:50:46 -0700

    Describe the update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ import congress from './data/congress.json';
 import countyFacilities from './data/countyFacilities.json';
 import stateAssembly from './data/stateAssembly.json';
 import stateSenate from './data/stateSenate.json';
-import schoolSites from './data/schoolSiteData.json'
+import schoolSites from './data/schoolSiteData.json';
+import privateSchools from './data/privateSchoolData.json';
 
 export default function App() {
   const [cardVisibility, setCardVisibility] = useState({
@@ -26,7 +27,8 @@ export default function App() {
     ZipCode: { visible: false, label: 'Zip Code' },
     Sup: { visible: false, label: 'County Supervisor' },
     SchoolDistrict: { visible: true, label: 'School District' },
-    schoolSites: { visible: true, label: 'School Sites' },
+    schoolSites: { visible: true, label: 'Public Schools' },
+    privateSchools: { visible: true, label: 'Private Schools' },
     WaterDistrict: { visible: true, label: 'Water District' },
     FireStation: { visible: true, label: 'Nearest Fire Station' },
     DeviceLocation: { visible: true, label: 'Device Location' },
@@ -76,8 +78,8 @@ export default function App() {
     { key: 'ZipCode', component: <LocationAnalyzer featureData={zipCodes.features} fields={[{ key: 'ZIP_CODE', label: 'Zip Code' }, { key: 'POPULATION', label: 'Population' }, { key: 'POP_SQMI', label: 'Pop. Per Sq. Mi.' }]} featureType="Zip Code" userLocationDataObj={userLocationDataObj} /> },
     { key: 'Sup', component: <LocationAnalyzer featureData={supData.features} fields={[{ key: 'NAME', label: 'County Supervisor' }, { key: 'Label', label: 'Area' }]} featureType="Supervisorial District" userLocationDataObj={userLocationDataObj} /> },
     { key: 'SchoolDistrict', component: <LocationAnalyzer featureData={schoolDistrictData.features} fields={[{ key: 'S_DISTRICT', label: 'School District' }]} featureType="School District" userLocationDataObj={userLocationDataObj} /> },
-    //{ key: 'schoolSites', component: <LocationAnalyzerClosest featureData={schoolSites} fields={[{ keyField: 'SchoolName', label: 'School Name' }]} featureType="Public School" userLocationDataObj={userLocationDataObj} /> },
     { key: 'schoolSites', component: <LocationAnalyzerClosest featureData={schoolSites} keyField="SchoolName" featureType="Public School" userLocationDataObj={userLocationDataObj} fields={[{key: 'SchoolName', label: 'School Name' },{ key: 'DistrictName', label: 'District Name' },{ key: 'SchoolType', label: 'School Type' },{ key: 'EnrollTotal', label: 'Enrollment' }]} /> },
+    { key: 'privateSchools', component: <LocationAnalyzerClosest featureData={privateSchools} keyField="School" featureType="Private School" userLocationDataObj={userLocationDataObj} fields={[{key: 'School', label: 'School Name' }]} /> },
     { key: 'WaterDistrict', component: <LocationAnalyzer featureData={waterDistrictData.features} fields={[{ key: 'SA_NAME', label: 'Water District' }]} featureType="Water District" userLocationDataObj={userLocationDataObj} /> },
     { key: 'FireStation', component: <LocationAnalyzerClosest featureData={fireStations} keyField="Alias" featureType="Fire Station" userLocationDataObj={userLocationDataObj} fields={[{key: 'SiteAddress', label: 'Address' },{ key: 'City', label: 'City' },{ key: 'Agency', label: 'Agency' },{ key: 'StationNumber', label: 'StationNumber' }]} /> },
     { key: 'countyFacilities', component: <LocationAnalyzerClosest featureData={countyFacilities} keyField="Name" featureType="Facility Name" userLocationDataObj={userLocationDataObj} fields={[{key: 'Name', label: 'Facility Name' },{ key: 'Agency', label: 'Agency' }]} /> },
